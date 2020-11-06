b16e5a2112e4ad0b85d774e6b31b52123fea268a
shrey_devep <shrey.devep@gmail.com>
2020-11-06 20:05:05 +0530

    Map User Location Detection Integrated(with Override)

--- components/maps.js
+++ components/maps.js
@@ -9,43 +9,61 @@ const locationList = [
   { lat: 28.5649, lng: 77.2403 },
   { lat: 28.4595, lng: 77.0266 },
 ];
+
+function getUserCurrentLocation(callback) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    window.console &&
+      console.log("coords : latitude=" + lat + ", longitude=" + lng);
+    callback(["coords", lat, lng]);
+  });
+}
+
 function initMap(API_KEY) {
   var platform = new H.service.Platform({
     apikey: API_KEY,
   });
 
   var defaultLayers = platform.createDefaultLayers();
-
   var map = new H.Map(
     document.getElementById("mapContainer"),
+
     defaultLayers.vector.normal.map,
     {
-      zoom: 12,
-      center: { lat: 28.7041, lng: 77.1025 },
+      zoom: 10,
+      center: { lat: lat, lng: lng },
     }
   );
 
-  var mapEvents = new H.mapevents.MapEvents(map);
-
-  var behavior = new H.mapevents.Behavior(mapEvents);
 
   function addMarkerToMap(props) {
     var hostPlaceMarker = new H.map.Marker(props.chords);
     map.addObject(hostPlaceMarker);
   }
 
-  window.addEventListener("resize", () => map.getViewPort().resize());
-
   window.onload = function () {
     locationList.forEach((element) => {
-      addMarkerToMap({ chords: element });
-    });
+    addMarkerToMap({ chords: element });
+    console.log("Check");
+      });
   };
+
+  var mapEvents = new H.mapevents.MapEvents(map);
+  var behavior = new H.mapevents.Behavior(mapEvents);
+  var ui = H.ui.UI.createDefault(map, defaultLayers, 'en-US');
 }
 
-initMap(API_KEY);
 
-map.addEventListener("tap", function (evt) {
-  // Log 'tap' and 'mouse' events:
-  console.log(evt.type, evt.currentPointer.type);
+var lat = 28.6139;
+var lng = 77.209;
+getUserCurrentLocation(function (callback) {
+  lat = callback[1];
+  lng = callback[2];
+
+  // Override for getting Delhi location
+
+  lat = 28.6139;
+  lng = 77.209;
 });
+initMap(API_KEY);
