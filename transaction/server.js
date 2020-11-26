95c064a1c6e47bb1ec32102f13874e4a9b3237f0
Ananddubey01 <kr.anand619@gmail.com>
2020-11-26 14:31:01 +0530

    payment, user transactions and reviews added

---
+++ transaction/server.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = "mongodb+srv://SleepyAnand:blabla@cluster0.qx1pd.mongodb.net/transaction"
+
+//mongoose.connect("mongodb+srv://SleepyAnand:blabla@cluster0.qx1pd.mongodb.net/transaction", {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
